0d8d0317eea4e79e78939c43f67395ec1712aa7a
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2015-12-13 12:28:47 +0200

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(name='micropython-filedb',
+      version='0.1',
+      description="""Very simple, filesystem-based ORM (Object-Relational
+Mapper) for MicroPython.
+""",
+      url='https://github.com/pfalcon/filedb',
+      author='Paul Sokolovsky',
+      author_email='pfalcon@users.sourceforge.net',
+      license='MIT',
+      py_modules=['filedb'])
