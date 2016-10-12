1f95a4f69bea04b670fb3e8edaa9d81aa816bf60
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-10-12 18:19:31 +0300

    Release 0.1.2.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='micropython-filedb',
-      version='0.1.1',
+      version='0.1.2',
       description="""Very simple, filesystem-based ORM (Object-Relational
 Mapper) for MicroPython.
 """,
